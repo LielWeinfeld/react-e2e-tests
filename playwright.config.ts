e0cb574c0072ee53d41aa0697069dab39bc03b0d
LielWein <lielweinfeld@mail.tau.ac.il>
2025-07-22 11:18:37 +0300

    Update playwright.config

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 
   use: {
   baseURL: "https://react.dev/",
-  headless: false,
+  headless: true,
   trace: "retain-on-failure",
   screenshot: "only-on-failure",
   video: "retain-on-failure",
